cbf499919e4d720cce90ab784a6a6db3c90f9f52
Mitchell Romney <mitchell@theearthissquare.com>
2019-11-08 09:50:48 +1100

    fix: add whitenoise

--- AllyBackend/settings.py
+++ AllyBackend/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AllyBackend.urls'
